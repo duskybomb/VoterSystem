03a668c082cc2be6ed92baa1738365cb33070591
duskybomb <harshit.joshi@outlook.com>
2018-04-18 00:25:45 +0530

    v2

---
+++ src/Candidate.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package votingui;
+
+/**
+ *
+ * @author harshit
+ */
+public class Candidate {
+    private String candName[] = new String[3];
+    private int candVotes[] = new int[3];
+    
+    public Candidate() {    
+        candName[0] = "Harshit Joshi";
+        candName[1] = "Manas Awasthi";
+        candName[2] = "Mayank Malik";
+    }
+    
+    public void cand1() {
+        candVotes[0]++;
+    }
+    public void cand2() {
+        candVotes[1]++;
+    }
+    public void cand3() {
+        candVotes[2]++;
+    }
+    
+    public int getcan1(){
+        return candVotes[0];
+    }
+
+    public int getcan2(){
+        return candVotes[1];
+    }
+
+    public int getcan3(){
+        return candVotes[2];
+    }
+}
--- src/Voter.java
+++ src/Voter.java
@@ -1,14 +1,42 @@
-import javax.swing.JFrame;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package votingui;
 
+/**
+ *
+ * @author harshit
+ */
 public class Voter {
-    public static void main(String[] a) {
-        LoginFrame frame = new LoginFrame();
-        frame.setTitle("Login Form");
-        frame.setVisible(true);
-        frame.setBounds(500, 100, 370, 600);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-
+    private String voterName[] = new String[3];
+    private int voterVotes[] = new int[3];
+    
+    public Voter() {    
+        voterName[0] = "Harshit Joshi";
+        voterName[1] = "Manas Awasthi";
+        voterName[2] = "Mayank Malik";
     }
-
+    
+    void voter1Voted() {
+        voterVotes[0] = 1;
+    }
+    void voter2Voted() {
+        voterVotes[1] = 1;
+    }
+    void voter3Voted() {
+        voterVotes[2] = 1;
+    }
+    public int getvoter1() {
+        return voterVotes[0];
+    }
+    public int getvoter2() {
+        return voterVotes[1];
+    }
+    public int getvoter3() {
+        return voterVotes[2];
+    }
+    
+    
 }
